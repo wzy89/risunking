3503b417aa4f06f5ae30f827509e1b256db4fbda
wangchaoyang <wangchaoyang@atwasoft.net>
2020-09-14 09:38:47 +0800

    修改部分代码添加新功能

--- risunking-server/src/main/java/com/wzy/risunking/resource/entity/ArticleInfo.java
+++ risunking-server/src/main/java/com/wzy/risunking/resource/entity/ArticleInfo.java
@@ -7,4 +7,86 @@ package com.wzy.risunking.resource.entity;
  * @date 2020/6/24 17:30
  */
 public class ArticleInfo {
+
+    private String id;
+    private String title;
+    private String content;
+    private String path;
+    private String url;
+    private String tagCodes;
+    private String tags;
+    private String readNum;
+    private String author;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public String getReadNum() {
+        return readNum;
+    }
+
+    public void setReadNum(String readNum) {
+        this.readNum = readNum;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getTagCodes() {
+        return tagCodes;
+    }
+
+    public void setTagCodes(String tagCodes) {
+        this.tagCodes = tagCodes;
+    }
 }
--- risunking-server/src/main/java/com/wzy/risunking/resource/entity/ArticleSearchIn.java
+++ risunking-server/src/main/java/com/wzy/risunking/resource/entity/ArticleSearchIn.java
@@ -7,4 +7,58 @@ package com.wzy.risunking.resource.entity;
  * @date 2020/4/23 17:21
  */
 public class ArticleSearchIn {
+    private String title;
+    private String createDate;
+    private String author;
+    private String tag;
+    private String page;
+    private String size;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getPage() {
+        return page;
+    }
+
+    public void setPage(String page) {
+        this.page = page;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
 }
